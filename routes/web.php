9cb9b6544f3c925e226692bc70377c8969d2d48b
Kévin <kevin.blaise24@gmail.com>
2023-03-06 12:52:35 +0100

    Add routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\StaticViewController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ErrorsController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +13,54 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Public routes
+require __DIR__ . '/auth.php';
+Route::get('/', [StaticViewController::class, 'homepage'])->name('homepage');
+
+// Authenticated routes
+Route::group([
+    'prefix' => 'dashboard',
+    'as' => 'dashboard.',
+    'middleware' => ['auth', 'verified', 'role:user|admin'],
+], function () {
+    Route::get('/', [DashboardController::class, 'create'])->name('index');
+
+    // Projects
+    Route::group([
+        'prefix' => 'projects',
+        'as' => 'projects.',
+        'controller' => ProjectsController::class
+    ], function () {
+        Route::get('/', 'list')->name('list');
+        Route::post('/new', 'create')->name('add');
+        Route::put('/edit/{project}', 'update')->name('edit');
+        Route::delete('/delete/{project}', 'delete')->name('delete');
+    });
+
+    // Errors
+    Route::group([
+        'prefix' => 'errors',
+        'as' => 'errors.',
+        'controller' => ErrorsController::class
+    ], function () {
+        Route::get('/', 'list')->name('list');
+        Route::put('/status/{error}', 'updateStatus')->name('update.status');
+        Route::put('/assign/{error}', 'assignTo')->name('update.assigned');
+    });
+
+    // Users
+    Route::group([
+        'prefix' => 'profil',
+        'as' => 'user.',
+        'controller' => UserController::class
+    ], function () {
+        Route::get('/', 'profil')->name('profil');
+        Route::put('/edit', 'update')->name('edit');
+        Route::delete('/delete', 'delete')->name('delete');
+    });
 });
