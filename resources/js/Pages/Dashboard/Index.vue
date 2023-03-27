934d14f3de698652b07e13a662af6402fdd69aa0
Oltean Petru <poltean33@gmail.com>
2023-03-27 17:27:35 +0200

    chart fix

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 import DefaultLayout from '@/Layouts/Default.vue';
+import StatsCard from '@/Components/StatsCard.vue';
+import { RectangleStackIcon, ExclamationTriangleIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 import { Line } from 'vue-chartjs';
 import {
     Chart as ChartJS,
@@ -51,13 +53,11 @@ const options = {
         },
     },
 };
-import StatsCard from '@/Components/StatsCard.vue';
-import { RectangleStackIcon, ExclamationTriangleIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 
 const statsCardsData = ref([
-    { title: "Total Projects", value: null, percentage: null, icon: RectangleStackIcon, iconBg: "bg-blue-500", clickable: true, path: "dashboard.projects.list" },
-    { title: "Total Errors", value: null, percentage: null, icon: ExclamationTriangleIcon, clickable: true, path: "dashboard.errors.list" },
-    { title: "Last 24h errors", value: null, percentage: null, icon: ChartBarIcon, iconBg: "bg-red-500", clickable: true, path: "dashboard.errors.list" },
+    { title: "Total projects", value: null, percentage: null, icon: RectangleStackIcon, iconBg: "bg-blue-500", clickable: true, path: "dashboard.projects.list" },
+    { title: "Total errors", value: null, percentage: null, icon: ExclamationTriangleIcon, iconBg: "bg-red-500", clickable: true, path: "dashboard.errors.list" },
+    { title: "24h Error count", value: null, percentage: null, icon: ChartBarIcon, clickable: true, path: "dashboard.errors.list" },
 ]);
 </script>
 
@@ -66,5 +66,6 @@ const statsCardsData = ref([
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
             <StatsCard v-for="(card, index) in statsCardsData" :key="index" v-bind="card" />
         </div>
+        <Line :data="data" :options="options" />
     </DefaultLayout>
 </template>
