920e25614522ccf8d76afcedcf0d5fd461f0f357
Kévin <kevin.blaise24@gmail.com>
2023-03-28 17:48:02 +0200

    Fix chart issue

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,17 +1,9 @@
 <script setup>
 import DefaultLayout from '@/Layouts/Default.vue';
 import StatsCard from '@/Components/StatsCard.vue';
+import Chart from '@/Components/Chart.vue';
 import { RectangleStackIcon, ExclamationTriangleIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 import { ref, watch } from 'vue';
-import { Line } from 'vue-chartjs';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip,
-} from 'chart.js';
 
 const props = defineProps({
     chartData: {
@@ -24,63 +16,20 @@ const props = defineProps({
     }
 });
 
-console.log('props', props.cardsData);
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip,
-);
-
 const selectedProject = ref('All');
-const chartRef = ref(null);
+const chartDataFormated = ref(props.chartData.allErrors);
 
 watch(selectedProject, (project) => {
-    chartRef.value.chart.clear();
-    if(project === 'All') {
-        data.value.datasets[0].data = props.chartData.allErrors;
-    } else {
-        data.value.datasets[0].data = project.errors
-    }
-    chartRef.value.chart.update();
-    console.log('kevin', chartRef.value)
-    console.log('data.value', data.value.datasets[0].data);
+    chartDataFormated.value = project === 'All'
+        ? props.chartData.allErrors
+        : project.errors;
 });
-
-
-const data = ref({
-    datasets: [
-        {
-            label: 'Error Count',
-            borderColor: '#ff8437',
-            cubicInterpolationMode: 'monotone',
-            data: props.chartData.allErrors,
-        },
-    ],
-});
-
-
-
-const options = {
-    scales: {
-        y: {
-            type: 'linear',
-            grace: '5%',
-        },
-        x: {
-            type: 'category',
-        },
-    },
-};
 
 const statsCardsData = ref([
     { title: "Total projects", value: ""+props.cardsData.projectCount, icon: RectangleStackIcon, iconBg: "bg-blue-500", clickable: true, path: "dashboard/projects" },
     { title: "Total errors", value: ""+props.cardsData.errorCount, icon: ExclamationTriangleIcon, iconBg: "bg-red-500", clickable: true, path: "dashboard/errors" },
     { title: "24h Error count", value: ""+props.cardsData.error24.count, percentage: props.cardsData.error24.percentage+"%", icon: ChartBarIcon, clickable: true, path: "dashboard/errors" },
 ]);
-
 </script>
 
 <template>
@@ -94,6 +43,6 @@ const statsCardsData = ref([
                 <option v-for="chartData in props.chartData.projectErrors" :key="chartData" :value="chartData">{{ chartData.project }}</option>
             </select>
         </div>
-        <Line ref="chartRef" :data="data" :options="options"  class="py-6 sm:px-6 px-2" />
+        <Chart :chartData="chartDataFormated" />
     </DefaultLayout>
 </template>
