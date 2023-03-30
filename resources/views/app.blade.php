65792f3aec403c771c2364e5bc422d568ae3de57
Kévin <kevin.blaise24@gmail.com>
2023-03-30 12:24:25 +0200

    Add analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,6 +25,22 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap">
 
+        <!-- Matomo -->
+        <script>
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+            var u="//analytics.dogger.host/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+        <!-- End Matomo Code -->
+
         <!-- Scripts -->
         @vite(['resources/scss/app.scss', 'resources/js/app.js'])
         @inertiaHead
